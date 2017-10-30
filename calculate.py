a566ed158e152aa6fa3ae4adb9914e54a6593ca0
xu zhipeng <1846987271@qq.com>
2017-10-30 16:53:16 +0800

    增加了计算的绘图程序

--- calculate.py
+++ calculate.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+import matplotlib
+from matplotlib import pyplot as plt
 import numpy as np
-# import matplotlib as plt
 
-a = np.array([1, 2, 3, 4])
-b = np.array([2, 4, 5, 6], dtype=np.float)
-print(b)
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
+matplotlib.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+
+years = np.linspace(1950, 2010, 7)
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+plt.plot(years, gdp, color="green", marker="o", linestyle="solid")
+plt.title("名义GDP")
+
+plt.ylabel("十亿美元")
+plt.show()
+
+
+my_mat = np.zeros([5, 4])
+print(my_mat)
