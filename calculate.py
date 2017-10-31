54f3b0825055fcedd3ea22013d1cf70d4be16c54
xu zhipeng <1846987271@qq.com>
2017-10-31 08:52:16 +0800

    增加了xlabel的显示，目的是测试中文字体的显示

--- calculate.py
+++ calculate.py
@@ -3,8 +3,8 @@ import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
-matplotlib.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']   # 指定默认字体
+matplotlib.rcParams['axes.unicode_minus'] = False   # 解决保存图像是负号'-'显示为方块的问题
 
 years = np.linspace(1950, 2010, 7)
 gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
@@ -13,6 +13,7 @@ plt.plot(years, gdp, color="green", marker="o", linestyle="solid")
 plt.title("名义GDP")
 
 plt.ylabel("十亿美元")
+plt.xlabel("年份")
 plt.show()
 
 
